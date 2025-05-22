0306d02a844780089e3426fb672e170e6c61c677
Peteroliver1 <72674757+Peteroliver1@users.noreply.github.com>
2025-05-22 16:16:29 +0300

    comment

--- phpWord.php
+++ phpWord.php
@@ -1,4 +1,5 @@
 <?php
+// PhpWord main class part of the PhpOffice\PhpWord lib.  Core component that allows you to create and manipulate Word documents programmatically.
 /**
  * This file is part of PHPWord - A pure PHP library for reading and writing
  * word processing documents.
